d98740c1e537d0efff0ad7d547f9021b94ed5f66
giovanni <giovanni@aimusic.co.uk>
2020-11-07 14:10:31 +0000

    refactor(style conf):

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-  ],
   root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/eslint-recommended', 'plugin:@typescript-eslint/recommended'],
   rules: {
-    '@typescript-eslint/parser.': 'off',
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-interface': ['off'],
+    '@typescript-eslint/interface-name-prefix': ['off'],
+    '@typescript-eslint/no-use-before-define': ['off'],
+    '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: true,
+        },
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': ['error'],
+    '@typescript-eslint/quotes': ['error', 'single', { allowTemplateLiterals: true }],
+    '@typescript-eslint/no-explicit-any': ['off'],
   },
-};
+  ignorePatterns: ['*.spec.ts', '*.test.ts', '**/__mocks__'],
+}
